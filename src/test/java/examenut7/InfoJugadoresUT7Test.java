3f0b671a3dbc6ed7ec9ca84bc257a4bc2a5e0225
juangarcia317 <juan.garcia317@educa.madrid.org>
2026-04-10 08:43:04 +0200

    cambios test

---
+++ src/test/java/examenut7/InfoJugadoresUT7Test.java
@@ -0,0 +1,14 @@
+package examenut7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InfoJugadoresUT7Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
